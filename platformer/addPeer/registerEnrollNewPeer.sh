b1dfa5686d2e832d63725e968bce339d11d74885
kartikTntra <rajput.kartik@tntra.io>
2023-03-08 10:55:26 +0000

    add peer in platformer

--- platformer/addPeer/registerEnrollNewPeer.sh
+++ platformer/addPeer/registerEnrollNewPeer.sh
@@ -1,34 +1,34 @@
-  export FABRIC_CA_CLIENT_HOME=${PWD}/../crypto-config-ca/peerOrganizations/org1.example.com/
+  export FABRIC_CA_CLIENT_HOME=${PWD}/../crypto-config-ca/peerOrganizations/platformer.com/
 
 registerPeer() {
   echo
   echo "Register peer2"
   echo
-  fabric-ca-client register --caname ca.org1.example.com --id.name peer2 --id.secret peer2pw --id.type peer --tls.certfiles ${PWD}/../fabric-ca/org1/tls-cert.pem
+  fabric-ca-client register --caname ca.platformer.com --id.name peer2 --id.secret peer2pw --id.type peer --tls.certfiles ${PWD}/../fabric-ca/platformer/tls-cert.pem
 
 }
 
 createMSPPeer2() {
   # Peer2
 
-  mkdir -p crypto-config-ca/peerOrganizations/org1.example.com/peers/peer2.org1.example.com
+  mkdir -p crypto-config-ca/peerOrganizations/platformer.com/peers/peer2.platformer.com
 
   echo
   echo "## Generate the peer2 msp"
   echo
-  fabric-ca-client enroll -u https://peer2:peer2pw@localhost:7054 --caname ca.org1.example.com -M ${PWD}/crypto-config-ca/peerOrganizations/org1.example.com/peers/peer2.org1.example.com/msp --csr.hosts peer2.org1.example.com --tls.certfiles ${PWD}/../fabric-ca/org1/tls-cert.pem
+  fabric-ca-client enroll -u https://peer2:peer2pw@localhost:7054 --caname ca.platformer.com -M ${PWD}/crypto-config-ca/peerOrganizations/platformer.com/peers/peer2.platformer.com/msp --csr.hosts peer2.platformer.com --tls.certfiles ${PWD}/../fabric-ca/platformer/tls-cert.pem
 
   sleep 5
-  cp ${PWD}/../crypto-config-ca/peerOrganizations/org1.example.com/msp/config.yaml ${PWD}/crypto-config-ca/peerOrganizations/org1.example.com/peers/peer2.org1.example.com/msp/config.yaml
+  cp ${PWD}/../crypto-config-ca/peerOrganizations/platformer.com/msp/config.yaml ${PWD}/crypto-config-ca/peerOrganizations/platformer.com/peers/peer2.platformer.com/msp/config.yaml
 
   echo
   echo "## Generate the peer2-tls certificates"
   echo
-  fabric-ca-client enroll -u https://peer2:peer2pw@localhost:7054 --caname ca.org1.example.com -M ${PWD}/crypto-config-ca/peerOrganizations/org1.example.com/peers/peer2.org1.example.com/tls --enrollment.profile tls --csr.hosts peer2.org1.example.com --csr.hosts localhost --tls.certfiles ${PWD}/../fabric-ca/org1/tls-cert.pem
+  fabric-ca-client enroll -u https://peer2:peer2pw@localhost:7054 --caname ca.platformer.com -M ${PWD}/crypto-config-ca/peerOrganizations/platformer.com/peers/peer2.platformer.com/tls --enrollment.profile tls --csr.hosts peer2.platformer.com --csr.hosts localhost --tls.certfiles ${PWD}/../fabric-ca/platformer/tls-cert.pem
   sleep 5
-  cp ${PWD}/crypto-config-ca/peerOrganizations/org1.example.com/peers/peer2.org1.example.com/tls/tlscacerts/* ${PWD}/crypto-config-ca/peerOrganizations/org1.example.com/peers/peer2.org1.example.com/tls/ca.crt
-  cp ${PWD}/crypto-config-ca/peerOrganizations/org1.example.com/peers/peer2.org1.example.com/tls/signcerts/* ${PWD}/crypto-config-ca/peerOrganizations/org1.example.com/peers/peer2.org1.example.com/tls/server.crt
-  cp ${PWD}/crypto-config-ca/peerOrganizations/org1.example.com/peers/peer2.org1.example.com/tls/keystore/* ${PWD}/crypto-config-ca/peerOrganizations/org1.example.com/peers/peer2.org1.example.com/tls/server.key
+  cp ${PWD}/crypto-config-ca/peerOrganizations/platformer.com/peers/peer2.platformer.com/tls/tlscacerts/* ${PWD}/crypto-config-ca/peerOrganizations/platformer.com/peers/peer2.platformer.com/tls/ca.crt
+  cp ${PWD}/crypto-config-ca/peerOrganizations/platformer.com/peers/peer2.platformer.com/tls/signcerts/* ${PWD}/crypto-config-ca/peerOrganizations/platformer.com/peers/peer2.platformer.com/tls/server.crt
+  cp ${PWD}/crypto-config-ca/peerOrganizations/platformer.com/peers/peer2.platformer.com/tls/keystore/* ${PWD}/crypto-config-ca/peerOrganizations/platformer.com/peers/peer2.platformer.com/tls/server.key
 
   # --------------------------------------------------------------------------------------------------
 
